d2c9763a764abcaed0c9a7d26fc018c97950e8e0
Thilan Senanayake <thilansenanayake@gmail.com>
2020-12-28 20:06:34 +0530

    Handled FileExistsError

--- main.py
+++ main.py
@@ -24,29 +24,32 @@ def rename_files(path):
 
         time.sleep(0.1)
 
-        if re.match(".*lookup.*", file):
-            os.rename(file, "lookup.csv")
-            print("Renamed", file, "to lookup.csv")
+        try:
+            if re.match(".*lookup.*", file):
+                os.rename(file, "lookup.csv")
+                print("Renamed", file, "to lookup.csv")
 
-        if re.match(".*keyword.*", file):
-            os.rename(file, "keyword.csv")
-            print("Renamed", file, "to keyword.csv")
+            if re.match(".*keyword.*", file):
+                os.rename(file, "keyword.csv")
+                print("Renamed", file, "to keyword.csv")
 
-        if re.match(".*intent_specific.*", file):
-            os.rename(file, "is.csv")
-            print("Renamed", file, "to is.csv")
+            if re.match(".*intent_specific.*", file):
+                os.rename(file, "is.csv")
+                print("Renamed", file, "to is.csv")
 
-        if re.match(".*label_split.*", file):
-            os.rename(file, "label_split.csv")
-            print("Renamed", file, "to label_split.csv")
+            if re.match(".*label_split.*", file):
+                os.rename(file, "label_split.csv")
+                print("Renamed", file, "to label_split.csv")
 
-        if re.match(".*basic.*", file):
-            if re.match(".*basic_regex.*", file):
-                os.rename(file, "basic_regex.csv")
-                print("Renamed", file, "to basic_regex.csv")
-            else:
-                os.rename(file, "basic.csv")
-                print("Renamed", file, "to basic.csv")
+            if re.match(".*basic.*", file):
+                if re.match(".*basic_regex.*", file):
+                    os.rename(file, "basic_regex.csv")
+                    print("Renamed", file, "to basic_regex.csv")
+                else:
+                    os.rename(file, "basic.csv")
+                    print("Renamed", file, "to basic.csv")
+        except FileExistsError:
+            pass
 
 
 class MyHandler(FileSystemEventHandler):
@@ -65,8 +68,8 @@ class MyHandler(FileSystemEventHandler):
 
     def on_deleted(self, event):
         super().on_deleted(event)
-        what = 'directory' if event.is_directory else 'file'
         print("Deleted", event.src_path)
+        rename_files(self.path)
 
     def on_modified(self, event):
         super().on_modified(event)
@@ -88,6 +91,6 @@ def watch_dir(path):
 
 if __name__ == '__main__':
     # path = os.getcwd() + '/test'
-    path = "F:\\github-projects\\file-renamer\\test"
+    dir_path = "F:\\github-projects\\file-renamer\\test"
     print("Listening...")
-    watch_dir(path)
+    watch_dir(dir_path)
