8fdcaa2c14d2150f63d38069e7cf22cc826ca2e8
Thilan Senanayake <thilansenanayake@gmail.com>
2021-07-01 23:56:53 +0530

    Read configuration from yaml and added multiple path support

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from threading import Thread
 
-from src.utils import read_json
+from src.utils import read_yaml
 from src.watcher import watch_dir
 
 
 if __name__ == '__main__':
 
-    configurations = read_json("rename_conf.json")
+    configurations = read_yaml("rename_conf.yml")
 
     for conf in configurations:
-        path = conf["path"]
-        Thread(target=watch_dir, args=(path, conf["renaming_rules"])).start()
+        renaming_rules = conf["renaming_rules"]
+
+        if "paths" in conf: # If multiple paths with same renaming rules
+            for path in conf["paths"]:
+                Thread(target=watch_dir, args=(path, renaming_rules)).start()
+
+        elif "path" in conf:
+            path = conf["path"]
+            Thread(target=watch_dir, args=(path, conf["renaming_rules"])).start()
--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,15 @@
-import json
 import yaml
 import os
 from os.path import dirname
 
 BASE_DIRECTORY = dirname(dirname(__file__))
 
-def read_json(file_path):
-    """ Reads json file and returns data in python dictionary format. """
-
-    with open(os.path.join(BASE_DIRECTORY, file_path), 'r') as json_file:
-        return json.load(json_file)
-
-
 def read_yaml(file_path):
     """
     Reads yaml file and returns data in python dictionary format.
     """
 
-    with open(file_path, 'r') as yaml_file:
+    with open(os.path.join(BASE_DIRECTORY, file_path), 'r') as yaml_file:
         try:
             return yaml.safe_load(yaml_file)
         except yaml.YAMLError as err:
